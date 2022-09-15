2d67bde4294050a668b234a1856bc507bcf71635
hector.romang <hector.romang@durango.gob.mx>
2022-09-15 16:57:02 -0500

    longitud y latitud

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -29,13 +29,14 @@ export class ZoomRangeComponent implements AfterViewInit {
   @ViewChild('mapazoom') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
   zoomLevel: number = 15;
+  center: [number, number] = [ -104.67026735003768 , 24.024761597415583 ]
 
   constructor() {  }
   ngAfterViewInit(): void {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://style/mapbox/streets-v11',
-      center: [ -104.67026735003768 , 24.024761597415583 ],
+      center: this.center,
       zoom: this.zoomLevel
     });
 
@@ -48,7 +49,13 @@ export class ZoomRangeComponent implements AfterViewInit {
       if( this.mapa.getZoom() > 18){
         this.mapa.zoomTo( 18 );
       }
-    })
+    });
+
+    this.mapa.on('move', (event) => {
+      const target = event.target;
+      const { lng, lat } = target.getCenter();
+      this.center = [ lng, lat ]
+    });
   }
 
   zoomOut(){
